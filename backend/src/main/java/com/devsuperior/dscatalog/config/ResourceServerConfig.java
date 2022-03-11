17f7edd85d0053c6aed8cdc8d9cd416e18d2253a
nicolasarruda <nlarruda@hotmail.com>
2022-03-10 21:22:39 -0300

    Filters

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -15,10 +15,10 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
-	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**"  };
+	
+	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**"};
 	
 	private static final String[] ADMIN = { "/users/**" };
 	
